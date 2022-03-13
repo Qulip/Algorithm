2b0bbf8a4ad42186e25dbe31ca642a347c182802
Qulip <ilkwon.ryu@gmail.com>
2022-03-13 23:51:21 +0900

    Update 22/03/13

---
+++ Beakjoon/BFS/beakjoon1963.java
@@ -0,0 +1,19 @@
+//https://www.acmicpc.net/problem/1963
+//Solved : 22/03/13
+
+import java.util.*;
+import java.io.*;
+
+class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+
+
+        bw.write(sb.toString());
+        bw.close();
+        br.close();
+    }
+}
