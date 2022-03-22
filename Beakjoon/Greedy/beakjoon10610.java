6f8668a0ceb7a1aec8352354a9fd576d21aeb16f
Qulip <ilkwon.ryu@gmail.com>
2022-03-22 23:51:36 +0900

    Update 22/03/22

---
+++ Beakjoon/Greedy/beakjoon10610.java
@@ -0,0 +1,18 @@
+//https://www.acmicpc.net/problem/10610
+//Solved : 22/03/22
+
+import java.util.*;
+import java.io.*;
+
+class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+
+
+        bw.close();
+        br.close();
+    }
+}
